23dab5080567d425d7678a2c685f044fd0129f7f
Chirag Nagpal <chiragn@cs.cmu.edu>
2020-10-27 01:13:33 -0400

    Update utilities.py

--- dsm/utilities.py
+++ dsm/utilities.py
@@ -14,7 +14,8 @@
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+# along with Deep Survival Machines.
+# If not, see <https://www.gnu.org/licenses/>.
 
 """Utility functions to train the Deep Survival Machines models"""
 
