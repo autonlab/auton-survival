6af6ac90229789f0eb5fbf04463f9b3090de463e
Chirag Nagpal <nagpalchirag1994@gmail.com>
2021-02-13 16:54:41 +0530

    modified:   dsm/utilities.py

--- dsm/utilities.py
+++ dsm/utilities.py
@@ -54,7 +54,8 @@ def pretrain_dsm(model, t_train, e_train, t_valid, e_valid,
 
   premodel = DeepSurvivalMachinesTorch(1, 1,
                                        dist=model.dist,
-                                       risks=model.risks)
+                                       risks=model.risks,
+                                       optimizer=model.optimizer)
   premodel.double()
 
   optimizer = get_optimizer(premodel, lr)
