e759e6c8a8e90d1ee4b9caf97b2b7ab282f1ae69
Chirag Nagpal <nagpalchirag1994@gmail.com>
2021-02-13 17:14:17 +0530

    modified:   dsm/utilities.py

--- dsm/utilities.py
+++ dsm/utilities.py
@@ -73,7 +73,7 @@ def pretrain_dsm(model, t_train, e_train, t_valid, e_valid,
     optimizer.step()
 
     valid_loss = 0
-    for r in range(model.risks): 
+    for r in range(model.risks):
       valid_loss += unconditional_loss(premodel, t_valid, e_valid, str(r+1))
     valid_loss = valid_loss.detach().cpu().numpy()
     costs.append(valid_loss)
